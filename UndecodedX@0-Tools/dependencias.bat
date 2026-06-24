f12c597f708ef102db2bcb37e437afaac1ac4c8d
Jzxzn. <283313569+Ozxmzxcpwe@users.noreply.github.com>
2026-06-24 16:01:25 -0500

    Delete UndecodedX@0-Tools directory
    mantenimiento

--- UndecodedX@0-Tools/dependencias.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Instalando dependencias...
-
-python -m pip install --upgrade pip
-python -m pip install requests discord.py aiohttp dnspython
-
-echo.
-echo Dependencias instaladas correctamente.
-pause
